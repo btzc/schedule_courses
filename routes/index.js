48faf2ca6eebc95d85f6808cbc9a98d6472a206e
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2017-09-04 16:00:16 -0700

    Inital post request to get uvic term list working. Added handlebars for templating language

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,9 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var json = require('JSON');
+var handlebars = require('handlebars');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-router.post('/test', function(req, res, next) {
-    var url = 'https://www.uvic.ca/BAN2P/bwckschd.p_disp_dyn_sched';
-    var options = {
-        url: url,
-        json: true
-    };
-    var body = postRequest( options );
-    var terms = parseBody( body );
-    var subjectList = getSubjectList( terms );
-
-    res.render('index', {title: 'Express' });
-});
-
-/*********** CONTROLLER METHODS **************/
+/*********** CONTROLLER METHODS START **************/
 var parseBody = function( string ) {
     var res = string.split('\n');
     var pattern = new RegExp('ID="term_input_id');
@@ -33,35 +16,54 @@ var parseBody = function( string ) {
     }
     return res;
 };
+/*********** CONTROLLER METHODS END **************/
 
-var getSubjectList = function( terms ) {
-
-    for(var term in terms) {
-        console.log(term);
-        /*var headers = {
-            'Content-type': 'application/x-www-form-urlencoded'
-        };
-        var form = {
-            'p_calling_proc': 'bwckschd.p_disp_dyn_sched',
-            'p_term': term
-        };
-        var options = {
-            url: url,
-            method: 'POST',
-            headers: headers,
-            form: form
-        };*/
-    }
-};
-
-var postRequest = function( options ) {
-    var respbody;
+/*********** ROUTES START **************/
+router.get('/', function(req, res, next) {
+    var url = 'https://www.uvic.ca/BAN2P/bwckschd.p_disp_dyn_sched';
+    var options = {
+        url: url
+    };
     request(options, function( error, response, body ) {
         if(!error && response.statusCode === 200) {
-
+            var terms = parseBody( body );
+            var fall, spring, summer;
+            for(var i = 0; i < terms.length; i++) {
+                if (terms[i].substring(4, 6) === '01') {
+                    spring = terms[i];
+                }
+                else if (terms[i].substring(4, 6) === '05') {
+                    summer = terms[i];
+                }
+                else if (terms[i].substring(4, 6) === '09') {
+                    fall = terms[i];
+                }
+                else {
+                    console.log('An error has occurred');
+                }
+            }
+            res.render('index', { title: 'Express', 'summer': summer, 'spring': spring, 'fall': fall });
         }
     });
-    return respbody;
-};
+});
+
+router.post('/test', function(req, res, next) {
+    var term = req.body.select_term;
+    console.log(term);
+    var headers = {
+        'Content-type': 'application/x-www-form-urlencoded'
+    };
+    var form = {
+        'p_calling_proc': 'bwckschd.p_disp_dyn_sched',
+        'p_term': term
+    };
+    var options = {
+        url: url,
+        method: 'POST',
+        headers: headers,
+        form: form
+    };
+});
+/*********** ROUTES END **************/
 
 module.exports = router;
