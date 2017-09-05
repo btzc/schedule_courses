02ba3e9bf08e3cbb67fc048c71647d730fe026a2
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2017-09-05 00:32:28 -0700

    Ability to get list of courses given a subject id

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var jQuery = require('cheerio');
+var async = require('async');
 
 /*********** CONTROLLER METHODS START **************/
 var parseTerms = function( string ) {
@@ -67,18 +68,30 @@ router.post('/test', function(req, res, next) {
         url: url,
         method: 'POST',
         headers: headers,
-        form: form,
-        json: true
+        form: form
     };
     request(options, function( error, response, body ) {
         if(!error && response.statusCode === 200) {
             body = jQuery(body).find('#subj_id').children();
             var subjects = parseSubjects( body );
-
+            console.log(subjects[0]);
+            console.log(term);
             url = 'https://www.uvic.ca/BAN1P/bwckschd.p_get_crse_unsec';
-            // var data = 'term_in=' + term + '&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=' + subject + '&sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a';
-
-            res.send({ title: 'Express' });
+            var data = 'term_in=' + term + '&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=' + subjects[0] + '&sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a';
+            options = {
+                url: url,
+                method: 'POST',
+                headers: headers,
+                form: data
+            };
+            request(options, function( err, res, b ) {
+                if(!err && res.statusCode === 200) {
+                    console.log(b);
+                }
+                else {
+                    console.log(err);
+                }
+            });
         }
     });
 });
