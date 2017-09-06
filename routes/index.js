5d7de68da3ebd000bc0c59094dc9621c901f5702
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2017-09-06 00:40:39 -0700

    Ability to get every courses information given course ID

--- routes/index.js
+++ routes/index.js
@@ -74,24 +74,68 @@ router.post('/test', function(req, res, next) {
         if(!error && response.statusCode === 200) {
             body = jQuery(body).find('#subj_id').children();
             var subjects = parseSubjects( body );
-            console.log(subjects[0]);
-            console.log(term);
+
             url = 'https://www.uvic.ca/BAN1P/bwckschd.p_get_crse_unsec';
-            var data = 'term_in=' + term + '&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=' + subjects[0] + '&sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a';
-            options = {
-                url: url,
-                method: 'POST',
-                headers: headers,
-                form: data
+            // var data = 'term_in=201709&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=ASL&sel_crse=&sel_title=&sel_schd=%&sel_insm=%&sel_from_cred=&sel_to_cred=&sel_camp=%&sel_levl=%&sel_ptrm=%&sel_instr=%&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a'
+            var data = {
+                term_in: '201801',
+                sel_subj: '',
+                sel_day: 'dummy',
+                sel_schd: '%',
+                sel_insm: '%',
+                sel_camp: '%',
+                sel_levl: '%',
+                sel_sess: 'dummy',
+                sel_instr: '%',
+                sel_ptrm: '%',
+                sel_attr: 'dummy',
+                sel_crse: '',
+                sel_title: '',
+                sel_from_cred: '',
+                sel_to_cred: '',
+                begin_hh: '0',
+                begin_mi: '0',
+                begin_ap: 'a',
+                end_hh: '0',
+                end_mi: '0',
+                end_ap: 'a'
             };
-            request(options, function( err, res, b ) {
-                if(!err && res.statusCode === 200) {
-                    console.log(b);
-                }
-                else {
-                    console.log(err);
-                }
-            });
+
+            var subjectData = {};
+
+            async.each(subjects,
+                function(subject, callback) {
+                    //var data = 'term_in=' + term + '&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=' + subject + '&sel_crse=&sel_title=&sel_schd=%&sel_insm=%&sel_from_cred=&sel_to_cred=&sel_camp=%&sel_levl=%&sel_ptrm=%&sel_instr=%&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a';
+                    var data = 'term_in=' + term + '&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=' + subject + '&sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a';
+                    options = {
+                        url: url,
+                        method: 'POST',
+                        headers: headers,
+                        form: data
+                    };
+                    request(options, function requestCallback( err, res, b ) {
+                        if(!err && res.statusCode === 200) {
+                            console.log(b);
+                            // append to a variable outside the each
+                            callback();
+                        }
+                        else if (err) {
+                            console.log(err);
+                            callback(err);
+                        }
+                        else {
+                            callback('error: status code ' + res.statusCode);
+                        }
+                    });
+                },
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log('this is done');
+                    }
+                });
         }
     });
 });
